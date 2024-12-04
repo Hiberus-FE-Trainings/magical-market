7f1409db8207754c884ec7858fac218cb52bf5c7
ivanIglesiasP <vanluarkwork@gmail.com>
2024-12-04 17:39:34 +0100

    feat: adding route for categories

---
+++ src/app/shop/[category]/page.tsx
@@ -0,0 +1,11 @@
+export default function MagicShop() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>🪄 Diagon Alley Magic Market</h1>
+      <p>
+        Welcome to the Magic Market, where you’ll find the most fascinating
+        items for witches, wizards, and all lovers of magic.
+      </p>
+    </div>
+  )
+}
